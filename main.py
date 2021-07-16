45c7fb69a3399a2b1ed2240c932cb0551c884f2b
kittyfur <kittyfur@protonmail.com>
2021-07-15 20:25:43 -0700

    jinja2 template substitution
    All strings are interpreted as jinja2 templates.
    Added a new 'set' key which sets a variable for use in templates.
    Modified one scenario to use these new features.
    
    Added types to function signatures when helpful

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os.path
 import random
 import shutil
 import textwrap
+from collections.abc import Iterator, Iterable
 from glob import glob
 
 import yaml
+from jinja2 import Template
 
 
 class Output:
@@ -21,7 +23,7 @@ class Output:
                                       self.width,
                                       subsequent_indent=self.__count_indent(unwrapped)):
                 print(line)
-        
+
     def start_paragraph(self):
         if self._para_written:
             print()
@@ -41,12 +43,12 @@ class Output:
 out = Output()
 
 
-def roll(sides):
+def roll(sides: int):
     return random.randint(1, sides)
 
 
 # Randomly choose one item from a list weighed by their odds
-def choose_one(choices):
+def choose_one(choices: Iterable[dict]):
     total_weight = sum([x['odds'] for x in choices])
 
     result = roll(total_weight)
@@ -61,14 +63,14 @@ def choose_one(choices):
 
 
 # Choose one weighted item or None depending on the odds (0-1.0)
-def maybe_choose_one(choices, odds: float):
+def maybe_choose_one(choices: Iterable[dict], odds: float):
     if random.random() < odds:
         return choose_one(choices)
     return None
 
 
 # Return all choices that passed a 0-100 roll under its odds
-def choose_many(choices, odds_adjustment=1.0):
+def choose_many(choices: Iterable[dict], odds_adjustment=1.0):
     chosen = []
     for choice in choices:
         if roll(100) <= choice['odds'] * odds_adjustment:
@@ -76,27 +78,34 @@ def choose_many(choices, odds_adjustment=1.0):
     return chosen
 
 
-def print_choices(heading, selection):
+def print_choices(heading: str, selection: list, env):
     if len(selection) > 0:
         out.start_paragraph()
         out.println(heading)
 
     for choice in selection:
         out.println('  - ' + choice['description'])
+        if 'set' in choice.keys():
+            eval_set(env, choice['set'])
 
 
-def describe_if_not_none(prefix, choice):
+def describe_if_not_none(prefix: str, choice: dict, env):
     if choice is not None and ('name' not in choice or choice['name'] != 'none'):
         out.start_paragraph()
         out.println(' - ' + prefix + ' ' + choice['description'])
+        if 'set' in choice.keys():
+            eval_set(env, choice['set'])
 
 
 class Inventory:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data: list[dict]):
+        self.data: list[dict] = data
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[dict]:
         return self.data.__iter__()
+
+    def __len__(self) -> int:
+        return self.data.__len__()
 
     def select_class(self, classname):
         return Inventory([x for x in self.data if x['class'] == classname])
@@ -162,7 +171,23 @@ def load(path):
         return yaml.safe_load(f)
 
 
-def run(scenario_names=['*'], desired_inventory=None,
+def interpolate(obj, env):
+    """ interpolate strings inside structures recursively """
+    if isinstance(obj, str):
+        template = Template(obj)
+        return template.render(env)
+    elif isinstance(obj, dict):
+        result = {}
+        for k, v in obj.items():
+            result[k] = interpolate(v, env)
+        return result
+    elif isinstance(obj, list):
+        return [interpolate(item, env) for item in obj]
+    else:
+        return obj
+
+
+def run(scenario_names=None, desired_inventory=None,
         inventory_file='inventory.yaml', scenario_path='scenarios/', seed=None):
     """ Build and print out a scenario.
 
@@ -179,6 +204,8 @@ def run(scenario_names=['*'], desired_inventory=None,
         seed (int): Pre-seed the random number generator so every run produces the same results.
     """
 
+    if scenario_names is None:
+        scenario_names = ['*']
     if desired_inventory is None:
         desired_inventory = []
 
@@ -186,8 +213,10 @@ def run(scenario_names=['*'], desired_inventory=None,
 
     files = []
     for name in scenario_names:
-        files += glob(os.path.join(scenario_path, '{}.yaml'.format(name)))
+        if name.split(".")[-1] != 'yaml':
+            name = name + ".yaml"
 
+        files += glob(os.path.join(scenario_path, name))
 
     if not files:
         out.println("Could not find matching scenario files")
@@ -205,32 +234,64 @@ def run(scenario_names=['*'], desired_inventory=None,
 
     scenario = choose_one(scenarios)
 
+    eval(desired_inventory, inventory, scenario)
+
+
+def eval(desired_inventory, inventory, scenario):
     out.println("Instructions:")
 
+    env = {}
+
     for instruction in scenario['instructions']:
+        # apply template substitutions
+        instruction = interpolate(instruction, env)
         description = instruction.get('description')
+
+        odds = instruction.get('odds', 100)
+
+        # if odds is a string, treat it as a variable
+        if isinstance(odds, str):
+            odds = env[odds]
+        if odds is None:
+            odds = 100
 
         # An instruction can alter the odds of a selection from choose_many.
         # Less than 100 will reduce the number of chosen items while
         # greater than 100 will increase the number of items chosen.
-        adjust_odds = instruction.get('odds', 100) / 100.0
+        adjust_odds = odds / 100.0
 
-        if 'choose_one' in instruction:
-            selection = inventory.select_by_instruction(instruction['choose_one'])
-            describe_if_not_none(description, selection.choose_one_or_required(desired_inventory, adjust_odds))
-        if 'choose_one_of' in instruction:
-            selection = Inventory(instruction['choose_one_of'])
-            describe_if_not_none(description, selection.choose_one_or_required(desired_inventory, adjust_odds))
-        elif 'choose_many' in instruction:
-            selection = inventory.select_by_instruction(instruction['choose_many'])
-            print_choices(description, selection.choose_many_including_required(desired_inventory, adjust_odds))
-        elif 'choose_many_of' in instruction:
-            selection = Inventory(instruction['choose_many_of'])
-            print_choices(description, selection.choose_many_including_required(desired_inventory, adjust_odds))
-        elif instruction.keys() <= {'description', 'odds'}:
-            if 'odds' not in instruction or (random.random() < adjust_odds):
-                out.start_paragraph()
-                out.println(description)
+        # Chance of displaying a description or setting a variable (in the absence of a 'choose' statement)
+        chance = random.random()
+
+        if 'description' in instruction:
+            if 'choose_one' in instruction:
+                selection = inventory.select_by_instruction(instruction['choose_one'])
+                describe_if_not_none(description, selection.choose_one_or_required(desired_inventory, adjust_odds), env)
+            if 'choose_one_of' in instruction:
+                selection = Inventory(instruction['choose_one_of'])
+                describe_if_not_none(description, selection.choose_one_or_required(desired_inventory, adjust_odds), env)
+            elif 'choose_many' in instruction:
+                selection = inventory.select_by_instruction(instruction['choose_many'])
+                print_choices(description, selection.choose_many_including_required(desired_inventory, adjust_odds), env)
+            elif 'choose_many_of' in instruction:
+                selection = Inventory(instruction['choose_many_of'])
+                print_choices(description, selection.choose_many_including_required(desired_inventory, adjust_odds), env)
+            elif 'set' in instruction:
+                if odds == 100 or (chance < adjust_odds):
+                    eval_set(env, instruction['set'])
+            elif instruction.keys() <= {'description', 'odds', 'set'}:
+                if odds == 100 or (chance < adjust_odds):
+                    out.start_paragraph()
+                    out.println(description)
+
+
+def eval_set(env, set_statement):
+    # "set: foo" sets the foo variable to True
+    # otherwise, assume it is a name and value dictionary
+    if isinstance(set_statement, str):
+        env[set_statement] = True
+    else:
+        env[set_statement['name']] = set_statement['value']
 
 
 if __name__ == '__main__':
@@ -243,11 +304,14 @@ if __name__ == '__main__':
                         help='path to the inventory file', default='inventory.yaml')
     parser.add_argument('--scenario-path', metavar='PATH',
                         help='the directory to read scenario files from', default='scenarios')
+    parser.add_argument('--seed',
+                        help='set an rng seed so the program generates the same scenario every time')
     parser.add_argument('scenarios', nargs='*', default=['*'],
                         help='choose scenarios by name. Wildcards (*) are supported.')
 
     args = parser.parse_args()
     run(scenario_names=args.scenarios,
-            desired_inventory=args.desired_items,
-            inventory_file=args.inventory_file,
-            scenario_path=args.scenario_path)
+        desired_inventory=args.desired_items,
+        inventory_file=args.inventory_file,
+        scenario_path=args.scenario_path,
+        seed=args.seed)
