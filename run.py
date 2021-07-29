9bf74f993003d1d1c1c7e237945f6805e9d27066
kittyfur <kittyfur@protonmail.com>
2021-07-29 08:02:14 -0700

    fix import path in run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 
 import os
 import sys
 
 sys.path.insert(0, os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'constraint'))
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 
 from constraint.cli import entry
 
-entry()
+if __name__ == '__main__':
+    entry()
